77e1900b62a20b6849e2ba8b2d76ba5caa37b892
kihyun940 <houng027@gmail.com>
2024-01-27 21:48:30 +0900

    Feat: 회원 리포지토리 개발 - MemberRepository 추가   - save 저장   - findOne 단일 조회   - findAll 리스트 조회   - findByName 이름으로 조회
      * JPQL 사용
      ex) em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
      - SQL은 from의 대상이 테이블을 대상으로 쿼리를 주지만 JPQL은 엔티티를 대상으로 쿼리를 준다.
    
    @Repository : 스프링 빈으로 등록, JPA 예외를 스프링 기반 예외로 예외 변환
    @PersistenceContext : 엔티티 매니저(EntityManager) 주입
    @PersistenceUnit : 엔티티 매니저 팩토리(EntityManagerFactory) 주입

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        List<Member> result = em.createQuery("select m from Member m", Member.class) // SQL은 from의 대상이 테이블을 대상으로 쿼리를 주지만 JPQL은 엔티티를 대상으로 쿼리를 준다.
+                .getResultList();
+        return result;
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
