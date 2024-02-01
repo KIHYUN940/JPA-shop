bed05f4b5f70fdd003be305750e522f48c6b8cd1
kihyun940 <houng027@gmail.com>
2024-02-01 11:24:55 +0900

    Feat: 회원 목록 조회 - 컨트롤러, 타임리프 html 추가
    - MemberController -> 회원 목록 조회 list() 추가
      - Model -> 모델이라는 객체를 통해 화면에 데이터를 전달
        - memberService.findMembers()를 리스트 받아서 모델 속성에 넣어서 데이터 전달
          - 여기서는 List<Member> 처럼 엔티티로 반환하지만 API로 만들 때는 절대 엔티티를 외부로 반환하면 안된다. - 템플릿 엔진에서는 사용해도 괜찮다. -> 서버 사이드로 돌리기 때문. / 엔티티 로직을 추가할 시 API 스펙도 변하고 패스워드 같은 정보 노출이 있을 수 있음 -> DTO를 사용
    
    - memberList.html 추가
      - <td th:text="${member.address?.city}"></td>
      -> 타임리프에서 ?를 사용하면 뒤에 데이터가 null 값일 때 null이라고 판단하고 넘어간다.
    
    ** 실무에서 엔티티는 핵심 비즈니스 로직만 가지고 있고 화면을 위한 로직은 없어야 한다.
    -> 화면이나 API에 맞는 폼 객체나 DTO를 사용

--- jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -39,5 +41,16 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+
+    @GetMapping("/members")
+    public String list(Model model) {  //모델이라는 객체를 통해 화면에 데이터를 전달
+        List<Member> members = memberService.findMembers();
+        /*
+        여기서는 엔티티로 반환하지만 API로 만들 때는 절대 엔티티를 외부로 반환하면 안된다. - 템플릿 엔진에서는 사용해도 괜찮다. 서버 사이드로 돌리기 때문.
+        / 엔티티 로직을 추가할 시 API 스펙도 변하고 패스워드 같은 정보 노출이 있을 수 있음 -> DTO를 사용 */
+        model.addAttribute("members", members);  // 인라인 단축키 ctrl + alt + N
+        return "members/memberList";
     }
 }
